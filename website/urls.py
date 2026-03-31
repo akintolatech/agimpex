4a0726a6cb7e4712a28d3978fe193b2862410d8c
akintolatechnologies <akintolatechnologies@gmail.com>
2026-03-31 19:02:58 +0100

    Beta v0.3.3: General Website Optimization

--- website/urls.py
+++ website/urls.py
@@ -5,7 +5,7 @@ app_name = "website"
 urlpatterns = [
     path('', views.index, name="index"),
     path('about/', views.about, name="about"),
-    path('contact/', views.contact, name="contact"),
+    # path('contact/', views.contact, name="contact"),
     path('services/', views.services, name="services"),
     path('discount/', views.discount, name="discount")
 ]

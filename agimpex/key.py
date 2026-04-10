0a87e4e2e418d4bafaa83a2fb64c2794e5026a6f
akintolatechnologies <akintolatechnologies@gmail.com>
2026-04-10 03:08:48 +0100

    Beta v1.2: Section header transltion to figma, message form impl, translate catalog, email integration

---
+++ agimpex/key.py
@@ -0,0 +1,2 @@
+email = 'zenitha1@zenith-achievers.com'
+password = 'Rpp$m2mLk,$1MQWR'
--- agimpex/settings.py
+++ agimpex/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
-import key
+from agimpex import key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "website.apps.WebsiteConfig",
     'orders.apps.OrdersConfig',
     'cart.apps.CartConfig',
-    'rosetta',
+    # 'rosetta',
     'shop.apps.ShopConfig',
     "administration.apps.AdministrationConfig",
     'django.contrib.admin',
